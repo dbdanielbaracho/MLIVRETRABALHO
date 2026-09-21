87092ff6854dc1abcaafe03a3f424d30e63ebe6d
dbdanielbaracho <dbdanielbaracho@gmail.com>
2026-09-21 15:32:29 -0300

    feat: register ratings API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,5 +8,6 @@ import { AuthService } from './auth.service';
 import { ProfessionalProfileController } from './professional-profile.controller';
 import { WorkAssignmentsController } from './work-assignments.controller';
 import { EarningsController } from './earnings.controller';
-@Module({ controllers: [HealthController, JobsController, AuthController, MeController, ProfessionalProfileController, WorkAssignmentsController, EarningsController], providers:[DatabaseService,AuthService] })
+import { RatingsController } from './ratings.controller';
+@Module({ controllers: [HealthController, JobsController, AuthController, MeController, ProfessionalProfileController, WorkAssignmentsController, EarningsController, RatingsController], providers:[DatabaseService,AuthService] })
 export class AppModule {}
