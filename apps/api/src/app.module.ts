5894fb6eab40ac8508cf18ee21e24f72a08ea3b0
dbdanielbaracho <dbdanielbaracho@gmail.com>
2026-09-21 13:20:20 -0300

    Profile v0.12: persistent professional profile API
    * feat: add authenticated professional profile API
    
    * feat: register professional profile API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,5 +5,6 @@ import { AuthController } from './auth.controller';
 import { MeController } from './me.controller';
 import { DatabaseService } from './database.service';
 import { AuthService } from './auth.service';
-@Module({ controllers: [HealthController, JobsController, AuthController, MeController], providers:[DatabaseService,AuthService] })
+import { ProfessionalProfileController } from './professional-profile.controller';
+@Module({ controllers: [HealthController, JobsController, AuthController, MeController, ProfessionalProfileController], providers:[DatabaseService,AuthService] })
 export class AppModule {}
