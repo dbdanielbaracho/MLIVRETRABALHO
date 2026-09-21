98a409ba645b3f2195ccde146bc9898a3783d0ec
dbdanielbaracho <dbdanielbaracho@gmail.com>
2026-09-21 20:53:21 -0300

    Workforce v0.67b: Talent Pools API
    * feat: restore Talent Pools API on current main
    
    * feat: register Talent Pools API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -19,6 +19,7 @@ import { NotificationsController } from './notifications.controller';
 import { ConversationsController } from './conversations.controller';
 import { TrustEventsController } from './trust-events.controller';
 import { PaymentEventsController } from './payment-events.controller';
+import { TalentPoolsController } from './talent-pools.controller';
 import { CompanyJobCreateController } from './company-job-create.controller';
 @Module({ controllers: [HealthController, JobsController, AuthController, MeController, ProfessionalProfileController, WorkAssignmentsController, EarningsController, WorkPassportController, RatingsController, CompanyJobsController, CompanyJobCreateController], providers:[DatabaseService,AuthService] })
 export class AppModule {}
