d48fd0c1cb6db805489f7b94adbdffd4e09fc8fe
dbdanielbaracho <dbdanielbaracho@gmail.com>
2026-09-21 16:19:07 -0300

    Trust v0.25b: verified assignment rating API
    * feat: restore verified ratings API on current main
    
    * feat: register ratings API after Work Passport merge

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -9,5 +9,6 @@ import { ProfessionalProfileController } from './professional-profile.controller
 import { WorkAssignmentsController } from './work-assignments.controller';
 import { EarningsController } from './earnings.controller';
 import { WorkPassportController } from './work-passport.controller';
-@Module({ controllers: [HealthController, JobsController, AuthController, MeController, ProfessionalProfileController, WorkAssignmentsController, EarningsController, WorkPassportController], providers:[DatabaseService,AuthService] })
+import { RatingsController } from './ratings.controller';
+@Module({ controllers: [HealthController, JobsController, AuthController, MeController, ProfessionalProfileController, WorkAssignmentsController, EarningsController, WorkPassportController, RatingsController], providers:[DatabaseService,AuthService] })
 export class AppModule {}
