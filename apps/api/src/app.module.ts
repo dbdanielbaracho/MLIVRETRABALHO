194dcef5f0e59448e1187c5bce2650439e657af9
dbdanielbaracho <dbdanielbaracho@gmail.com>
2026-09-21 01:29:04 -0300

    identity: register auth API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { JobsController } from './jobs.controller';
-
-@Module({ controllers: [HealthController, JobsController] })
+import { AuthController } from './auth.controller';
+@Module({ controllers: [HealthController, JobsController, AuthController] })
 export class AppModule {}
