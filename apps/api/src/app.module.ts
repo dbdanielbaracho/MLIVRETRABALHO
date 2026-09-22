8d9f23a92d95ee47ebb90c55871b57fa8b2792a2
dbdanielbaracho <dbdanielbaracho@gmail.com>
2026-09-21 21:49:31 -0300

    feat: register safety review API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -22,6 +22,7 @@ import { PaymentEventsController } from './payment-events.controller';
 import { TalentPoolsController } from './talent-pools.controller';
 import { TeamAllocationController } from './team-allocation.controller';
 import { PaymentWebhookController } from './payment-webhook.controller';
+import { SafetyAdminController } from './safety-admin.controller';
 import { CompanyJobCreateController } from './company-job-create.controller';
 @Module({ controllers: [HealthController, JobsController, AuthController, MeController, ProfessionalProfileController, WorkAssignmentsController, EarningsController, WorkPassportController, RatingsController, CompanyJobsController, CompanyJobCreateController], providers:[DatabaseService,AuthService] })
 export class AppModule {}
